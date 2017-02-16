9975ac85acc9ecd1106429433905f255d820671a
Boinali <n.boinali@intech-so.fr>
2017-02-16 11:38:01 +0100

    deploiment dolicom_v2

---
+++ README.md
@@ -0,0 +1,4 @@
+dolicom_v2
+==========
+
+A Symfony project created on February 15, 2017, 5:39 am.
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,84 @@
+<?php
+// src/AppBundle/Entity/User.php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="dc_user")
+ */
+class User extends BaseUser
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+           
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+ 
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="github_id", type="string", nullable=true)
+     */
+    private $github_id;
+
+    private $githubAccessToken;
+
+    /**
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $github_id
+     * @return User
+     */
+    public function setgithub_id($facebookId)
+    {
+        $this->github_id = $github_id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getgithub_id()
+    {
+        return $this->github_id;
+    }
+
+    /**
+     * @param string $githubAccessToken
+     * @return User
+     */
+    public function setgithubAccessToken($githubAccessToken)
+    {
+        $this->githubAccessToken = $githubAccessToken;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getgithubAccessToken()
+    {
+        return $this->githubAccessToken;
+    }
+}
