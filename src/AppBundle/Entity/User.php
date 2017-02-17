96ca17ca21536f881ba8e0cfe1300ef53b084fd9
Nadjidine BOINALI <n.boinali@intech-so.fr>
2017-02-17 11:29:07 +0100

    Updtae User.php

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -5,6 +5,7 @@ namespace AppBundle\Entity;
 
 use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Contraints  as Assert;
 
 /**
  * @ORM\Entity
@@ -79,5 +80,121 @@ class User extends BaseUser
     public function getgithubAccessToken()
     {
         return $this->githubAccessToken;
+    }
+
+
+    /* dolibarr attribute*/
+
+    /**
+     * @var string
+     * @ORM\Column(name="dLogin", type="varchar", nullable=true, Length=24)
+     * @Assert\NotBlank
+     */
+    protected $dLogin;
+    /**
+     * @var string
+     */
+    protected $dMDP;
+    /**
+     * @var string
+     * @ORM\Column(name="dFirstname", type="varchar", nullable=true, Length=50)
+     * @Assert\NotBlank
+     */
+    protected $dFirstname;
+    /**
+     * @var string
+     * @ORM\Column(name="dLastname", type="varchar", nullable=true, Length=50)
+     */
+    protected $dLastname;
+    /**
+     * @var string
+     * @ORM\Column(name="dEmail", type="varchar", nullable=true, Length=255)
+     * @Assert\Email
+     */
+    protected $dEmail;
+
+    /* Getters & Setters */
+
+    /**
+     * @return string
+     */
+    public function getDLogin()
+    {
+        return $this->dLogin;
+    }
+    /**
+     * @param string $dLogin
+     * @return user
+     */
+    public function setDLogin($dLogin)
+    {
+        $this->dLogin = $dLogin;
+        return $this;
+    }
+    /**
+     * @return string
+     */
+    public function getDMDP()
+    {
+        return $this->dMDP;
+    }
+    /**
+     * @param string $dMDP
+     * @return user
+     */
+    public function setDMDP($dMDP)
+    {
+        $this->dMDP = $dMDP;
+        return $this;
+    }
+    /**
+     * @return string
+     */
+    public function getDFirstname()
+    {
+        return $this->dFirstname;
+    }
+    /**
+     * @param string $dFirstname
+     * @return user
+     */
+    public function setDFirstname($dFirstname)
+    {
+        $this->dFirstname = $dFirstname;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDLastname()
+    {
+        return $this->dLastname;
+    }
+    /**
+     * @param string $dLastname
+     * @return user
+     */
+    public function setDLastname($dLastname)
+    {
+        $this->dLastname = $dLastname;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDEmail()
+    {
+        return $this->dEmail;
+    }
+    /**
+     * @param string $dEmail
+     * @return user
+     */
+    public function setDEmail($dEmail)
+    {
+        $this->dEmail = $dEmail;
+        return $this;
     }
 }
