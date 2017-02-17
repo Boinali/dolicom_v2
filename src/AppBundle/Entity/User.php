ad5002ed9276ba8587245422fbfadfe40d2258c0
Nadjidine BOINALI <n.boinali@intech-so.fr>
2017-02-17 11:44:22 +0100

    Updtae User.php

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -5,7 +5,7 @@ namespace AppBundle\Entity;
 
 use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Contraints  as Assert;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
