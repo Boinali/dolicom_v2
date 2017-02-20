324e81c4d73e102ac5f0c1a93a3de8a39e0ddc1b
Nadjidine BOINALI <n.boinali@intech-so.fr>
2017-02-20 16:40:50 +0100

    test API rest

--- src/AppBundle/Controller/Api/dolibController.php
+++ src/AppBundle/Controller/Api/dolibController.php
@@ -43,6 +43,6 @@ class dolibController extends Controller
 
     public function getAllUsers()
     {
-        return new Response('Hello World');
+        return new Response('http://dolibarr.localdomain/api/index.php/user/{n0}?api_key=712f3b895ada9274714a881c2859b617&id=1');
     }
 }
