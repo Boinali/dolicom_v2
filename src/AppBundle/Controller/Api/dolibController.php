e5feb13c7fddbe50768c0330efd60a8862ab9b99
Nadjidine BOINALI <n.boinali@intech-so.fr>
2017-02-20 17:55:36 +0100

    test API rest

--- src/AppBundle/Controller/Api/dolibController.php
+++ src/AppBundle/Controller/Api/dolibController.php
@@ -45,7 +45,7 @@ class dolibController extends Controller
     {
 
         $users = $this->get('doctrine.orm.customer_entity_manager')
-            ->getRepository('AppBundle:dolibUser')
+            ->getRepository('AppBundle:')
             ->findAll();
         /* @var $users User[] */
 
