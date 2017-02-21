c5c2c67843f8326b21d8aadeb5cfc40831da9aa0
Nadjidine BOINALI <n.boinali@intech-so.fr>
2017-02-21 17:50:33 +0100

    test API rest

--- src/AppBundle/Controller/Api/dolibController.php
+++ src/AppBundle/Controller/Api/dolibController.php
@@ -33,7 +33,7 @@ class dolibController extends Controller
         $buzz = $this->container->get('buzz');
 //        var_dump('here');die();
         $browser = $buzz->getBrowser('dolibarr');
-        $response = $browser->get('/toto');
+        $response = $browser->get('/{n0}?api_key=712f3b895ada9274714a881c2859b617&id=1');
 
         echo $browser->getLastRequest()."\n";
         echo $response;
