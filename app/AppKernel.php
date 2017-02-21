9ec16676ab51a6878e03a33d0b1beff2b67be104
Boinali <n.boinali@intech-so.fr>
2017-02-21 15:24:58 +0100

    install buzz

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,8 +19,8 @@ class AppKernel extends Kernel
 
             new FOS\UserBundle\FOSUserBundle(),
             new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
-            new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
-            new Buzz\Bundle\BuzzBundle\BuzzBundle(),
+          //  new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
+          //  new Buzz\Bundle\BuzzBundle\BuzzBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
