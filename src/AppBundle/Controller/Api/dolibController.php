0452649c36d1a5fc33df1913105f95a898b317c8
Nadjidine BOINALI <n.boinali@intech-so.fr>
2017-02-22 17:04:34 +0100

    test API rest

--- src/AppBundle/Controller/Api/dolibController.php
+++ src/AppBundle/Controller/Api/dolibController.php
@@ -56,6 +56,7 @@ class dolibController extends Controller
         $mp = 112;
         $isArive = $response->getStatusCode();
         if($isArive != 200){
+            $browser->get('/');
             $response = null;
             $content = null;
             return $this->render('list_dolib_users.html.twig',
