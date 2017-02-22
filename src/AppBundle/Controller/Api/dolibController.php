30be17fdf40d03acb0d73cb764e5bdb316db45ee
Nadjidine BOINALI <n.boinali@intech-so.fr>
2017-02-22 10:53:42 +0100

    test API rest

--- src/AppBundle/Controller/Api/dolibController.php
+++ src/AppBundle/Controller/Api/dolibController.php
@@ -35,11 +35,11 @@ class dolibController extends Controller
         $browser = $buzz->getBrowser('dolibarr');
         $response = $browser->get('/{n0}?api_key=712f3b895ada9274714a881c2859b617&id=1');
 
-        dump($browser->getLastRequest());
-        dump($response);
+//        dump($browser->getLastRequest());
+//        dump($response);
 
         return $this->render('list_dolib_users.html.twig',
-            array('response' => $response)
+            array('response' => $response{'content'})
         );
 
     }
