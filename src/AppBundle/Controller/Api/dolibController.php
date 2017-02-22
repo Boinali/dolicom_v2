41968fd2eed0dc617b98f45ed0cf16253b9e761d
Nadjidine BOINALI <n.boinali@intech-so.fr>
2017-02-22 17:22:03 +0100

    test API rest

--- src/AppBundle/Controller/Api/dolibController.php
+++ src/AppBundle/Controller/Api/dolibController.php
@@ -52,6 +52,7 @@ class dolibController extends Controller
         if(!$response = $browser->get('/{n0}?api_key=712f3b895ada9274714a881c2859b617&id='.$id.'')->isSuccessful()){
             die();
         }
+        else{var_dump('toto');die();}
         // verification de la requete
         $isArive = $response->getStatusCode();
         if($isArive != 200){
