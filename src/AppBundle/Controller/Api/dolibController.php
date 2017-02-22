59d6af295d4553813eab6a28a6f74694cf67e5ff
Nadjidine BOINALI <n.boinali@intech-so.fr>
2017-02-22 15:23:25 +0100

    test API rest

--- src/AppBundle/Controller/Api/dolibController.php
+++ src/AppBundle/Controller/Api/dolibController.php
@@ -32,22 +32,22 @@ class dolibController extends Controller
      * @Method("GET")
      */
 
-//    public function getUser()
-//    {
-//        $buzz = $this->container->get('buzz');
-////        var_dump('here');die();
-//        $id = 21;
-//        $browser = $buzz->getBrowser('dolibarr');
-//        $response = $browser->get('/{n0}?api_key=712f3b895ada9274714a881c2859b617&id='.$id.'');
-//
-////        dump($browser->getLastRequest());
-//        dump($response);
-//        $content = json_decode($response->getContent());
-//        return $this->render('list_dolib_users.html.twig',
-//            array('response' => $content)
-//        );
-//
-//    }
+    public function getUser()
+    {
+        $buzz = $this->container->get('buzz');
+//        var_dump('here');die();
+        $id = 21;
+        $browser = $buzz->getBrowser('dolibarr');
+        $response = $browser->get('/{n0}?api_key=712f3b895ada9274714a881c2859b617&id='.$id.'');
+
+//        dump($browser->getLastRequest());
+        dump($response);
+        $content = json_decode($response->getContent());
+        return $this->render('list_dolib_users.html.twig',
+            array('response' => $content)
+        );
+
+    }
 
     public function findByID(Request $request)
     {
