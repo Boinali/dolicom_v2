15c965a9835c8d00917eba7b4b5875a1a4be4dae
Boinali <n.boinali@intech-so.fr>
2017-02-23 10:42:50 +0100

    integration Bootstrap & font-awesome

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,7 +20,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
           //  new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
-          //  new Buzz\Bundle\BuzzBundle\BuzzBundle(),
+            new Buzz\Bundle\BuzzBundle\BuzzBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
