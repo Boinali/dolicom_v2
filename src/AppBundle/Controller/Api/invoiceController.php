3f23f812bfd69e16ef9cbc123d0c45f504213854
Nadjidine BOINALI <n.boinali@intech-so.fr>
2017-02-24 14:00:45 +0100

    test API rest

--- src/AppBundle/Controller/Api/invoiceController.php
+++ src/AppBundle/Controller/Api/invoiceController.php
@@ -12,13 +12,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-use Buzz\Form;
+
 use Buzz\Message;
 use Buzz\Client\ClientInterface;
 use Buzz\Client\FileGetContents;
 use Buzz\Listener\ListenerChain;
 use Buzz\Listener\ListenerInterface;
-use Buzz\Listener\CallbackListener;
 use Buzz\Message\Factory\Factory;
 use Buzz\Message\Factory\FactoryInterface;
 use Buzz\Message\RequestInterface;
@@ -38,76 +37,10 @@ class invoiceController extends Controller
     /**
      * @Route("/api/invoices", name = "dolib_invoices")
      */
-//     public function createInvoice(Request $request)
-//     {
-//
-//          $socid = null;
-//          $form = $this->createFormBuilder()
-////              ->add('Libelle', TextType::class)
-//              ->add('Client_Id', TextType::class)
-//              ->add('save', SubmitType::class, array('label' => 'créer'))
-//              ->getForm();
-//
-//          $form->handleRequest($request);
-//
-//          if ($form->isSubmitted() && $form->isValid())
-//          {
-//              $headers = ['Content-Type', 'application/json'];
-//               // recuperation des données Post
-//               $data = $form->getData();
-//               $socid = $data['Client_Id'];
-//                $api_url = "http://dolibarr.localdomain/api/index.php/invoice/?api_key=712f3b895ada9274714a881c2859b617";
-//               $invoiceContent["socid"] = $socid;
-//               $invoiceContent = json_encode($invoiceContent);
-//
-//               // envoie de la requette -> creation facture
-//
-//               $buzz = $this->container->get('buzz');
-//               $browser = $buzz->getBrowser('dolibarr');
-//               $response = $browser->preSend('/invoice/?api_key=712f3b895ada9274714a881c2859b617');
-//
-//              var_dump($response);die();
-//              return $this->render('invoices.html.twig',
-//                  array(
-////                       'Libelle' => $libelle,
-//                      'Client_Id' => $socid,
-//                      'form' => $form->createView(),)
-//              );
-////               return $this->redirectToRoute('dolib_invoices');
-//          }
-//
-//          return $this->render('invoices.html.twig',
-//              array(
-//                  'Client_Id' => $socid,
-//                  'form' => $form->createView(),)
-//          );
-//     }
-
-
-    public  function preSend()
-    {
-        $jsonPayload = json_encode([
-                        "socid" => "2",
-                    ]);
-        $headers = ['Content-Type', 'application/json'];
-
-        $request = new Form(Form::METHOD_POST, "/invoice/?api_key=712f3b895ada9274714a881c2859b617", "api.website.com");
-        $request->setFields([
-            "socid" => "bugsBunny"
-        ]);
-
-        $response = new Buzz\Message\Response();
-
-        $client = new Buzz\Client\Curl();
-        $client->send($request, $response);
-
-        $browser = new Buzz\Browser();
-        $response = $browser->post('http://dolibarr.localdomain/api/index.php/invoice/?api_key=712f3b895ada9274714a881c2859b617', $headers, $jsonPayload);
-
-        var_dump($response);die();
-        return $this->render('invoices.html.twig'
-        );
-    }
+     public function createInvoice(Request $request)
+     {
+         return $this->render('list_dolib_users.html.twig');
+     }
     // création invoices
 
 }
