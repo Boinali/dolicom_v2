26a1f2b819b5809f6779fea80320d54708bb05d1
Nadjidine BOINALI <n.boinali@intech-so.fr>
2017-02-27 16:32:59 +0100

    test API rest

--- src/AppBundle/Controller/Api/categoryController.php
+++ src/AppBundle/Controller/Api/categoryController.php
@@ -8,6 +8,30 @@
 
 namespace AppBundle\Controller\Api;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+//use AppBundle\Buzz\Listener\TokenListener;
+
+use Buzz\Message;
+use Buzz\Client\ClientInterface;
+use Buzz\Client\FileGetContents;
+use Buzz\Listener\ListenerChain;
+use Buzz\Listener\ListenerInterface;
+use Buzz\Message\Factory\Factory;
+use Buzz\Message\Factory\FactoryInterface;
+use Buzz\Message\RequestInterface;
+use Buzz\Util\Url;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class categoryController extends Controller
 {
