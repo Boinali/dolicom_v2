2171474ebf28416dbe8a70802f3f13542537edff
Nadjidine BOINALI <n.boinali@intech-so.fr>
2017-02-27 10:42:16 +0100

    test API rest

--- src/AppBundle/Controller/Api/invoiceController.php
+++ src/AppBundle/Controller/Api/invoiceController.php
@@ -51,7 +51,7 @@ class invoiceController extends Controller
 //            $data = $form->getData();
              $buzz = $this->container->get('buzz');
              $browser = $buzz->getBrowser('dolibarr');
-             var_dump($browser->setContent('toto'));
+             var_dump($browser->setContent('toto'));die();
              $response = $browser->post('/invoice/?api_key=712f3b895ada9274714a881c2859b617');
          }
 
