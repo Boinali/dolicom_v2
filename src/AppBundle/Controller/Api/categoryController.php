b0c474bb8f10461f6796754ff8a51732a4ddc862
Nadjidine BOINALI <n.boinali@intech-so.fr>
2017-02-28 11:57:41 +0100

    test API rest

--- src/AppBundle/Controller/Api/categoryController.php
+++ src/AppBundle/Controller/Api/categoryController.php
@@ -62,7 +62,7 @@ class categoryController extends Controller
 
         if('POST' === $request->getMethod()){
             // traitement du premier form
-            var_dump($request->request->has('form'));
+            var_dump($request->request->has('formCreateCat'));
             dump($request->request->all());
             if ($request->request->has('formCreateCat')){
 
