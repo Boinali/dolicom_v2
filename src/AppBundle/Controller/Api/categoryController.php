b5298491070cfd9a771f00a1b40be192ddd1c428
Nadjidine BOINALI <n.boinali@intech-so.fr>
2017-02-28 14:07:51 +0100

    test API rest

--- src/AppBundle/Controller/Api/categoryController.php
+++ src/AppBundle/Controller/Api/categoryController.php
@@ -131,19 +131,14 @@ class categoryController extends Controller
 
     }
 
-    public function createServices(Request $request)
-    {
-        $form = $this->createFormBuilder()
-            ->add('Label', TextType::class)
-            ->add('Type', TextType::class)
-            ->add('Color', TextType::class)
-            ->add('save', SubmitType::class, array('label' => 'créer'))
-            ->getForm();
-
-        return $this->render('categories.html.twig',
-            array(
-                'form' => $form->createView(),
-            )
-        );
-    }
+//    public function createServices(Request $request)
+//    {
+//        // recup des produits et services
+//
+//        return $this->render('categories.html.twig',
+//            array(
+//                'form' => $form->createView(),
+//            )
+//        );
+//    }
 }
