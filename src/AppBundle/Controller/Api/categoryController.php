52cd6bdf228406d39c3bad0e40fb4ecdc9e232a7
Nadjidine BOINALI <n.boinali@intech-so.fr>
2017-03-01 15:22:37 +0100

    classement des dossier

--- src/AppBundle/Controller/Api/categoryController.php
+++ src/AppBundle/Controller/Api/categoryController.php
@@ -8,6 +8,7 @@
 
 namespace AppBundle\Controller\Api;
 
+use MongoDB\BSON\Regex;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -56,7 +57,7 @@ class categoryController extends Controller
             ))
             ->add('Type', NumberType::class)
             ->add('Color', TextType::class,array(
-                'pattern' => '/^#[0-9a-f]{3,6}$/i',
+                'pattern' => '/^[0-9a-f]{3,6}$/i',
                 "required" => false))
             ->getForm();
 
