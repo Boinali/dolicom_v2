aa5cdb785558c6fd09a5d839d4ec655d277b0668
Nadjidine BOINALI <n.boinali@intech-so.fr>
2017-03-02 03:55:07 +0100

    classement des dossier

--- src/AppBundle/Controller/Api/categoryController.php
+++ src/AppBundle/Controller/Api/categoryController.php
@@ -156,7 +156,6 @@ class categoryController extends Controller
                 $status = $formCreateServ->get('Vente')->getData();
                 $status_buy = $formCreateServ->get('Achat')->getData();
                 $price_TTC = $formCreateServ->get('Prix_TTC')->getData();
-                var_dump($price_TTC);die();
                 $content["ref"] = $ref;
                 $content["label"] = $label;
                 $content["description"] = $desc;
