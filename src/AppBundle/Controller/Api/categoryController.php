6a108b2ae1391a5ec7e1086c019c8419ea59ac57
Nadjidine BOINALI <n.boinali@intech-so.fr>
2017-03-02 04:37:54 +0100

    classement des dossier

--- src/AppBundle/Controller/Api/categoryController.php
+++ src/AppBundle/Controller/Api/categoryController.php
@@ -192,7 +192,7 @@ class categoryController extends Controller
 
     }
 
-    public function getListAction(Request $request)
+    public function getListAction()
     {
         // recup des produits et services
         $buzz = $this->container->get('buzz');
@@ -201,7 +201,7 @@ class categoryController extends Controller
         $response = $browser->get('/api/index.php/product/list/?api_key=712f3b895ada9274714a881c2859b617');
 
         $contentList = json_decode($response->getContent());
-        var_dump($contentList);
+        var_dump($contentList);die();
         return $this->render('AppBundle::services.html.twig');
     }
 }
