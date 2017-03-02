1a45c78d1450f1cdb21f649b420155456b1f64f4
Nadjidine BOINALI <n.boinali@intech-so.fr>
2017-03-02 04:34:12 +0100

    classement des dossier

--- src/AppBundle/Controller/Api/categoryController.php
+++ src/AppBundle/Controller/Api/categoryController.php
@@ -192,14 +192,16 @@ class categoryController extends Controller
 
     }
 
-//    public function createServices(Request $request)
-//    {
-//        // recup des produits et services
-//
-//        return $this->render('services.html.twig',
-//            array(
-//                'form' => $form->createView(),
-//            )
-//        );
-//    }
+    public function getListAction(Request $request)
+    {
+        // recup des produits et services
+        $buzz = $this->container->get('buzz');
+
+        $browser = $buzz->getBrowser('dolibarr');
+        $response = $browser->get('/api/index.php/product/list/?api_key=712f3b895ada9274714a881c2859b617');
+
+        $contentList = json_decode($response->getContent());
+        var_dump($contentList);
+        return $this->render('AppBundle::services.html.twig');
+    }
 }
