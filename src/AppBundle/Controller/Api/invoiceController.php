7153b09fc48f314b9c91f23f564db5d7d698b8ca
Nadjidine BOINALI <n.boinali@intech-so.fr>
2017-03-03 11:35:23 +0100

    classement des dossier

--- src/AppBundle/Controller/Api/invoiceController.php
+++ src/AppBundle/Controller/Api/invoiceController.php
@@ -73,7 +73,7 @@ class invoiceController extends Controller
                  $browser = $buzz->getBrowser('dolibarr');
                  $response = $browser->submit('/invoice/?api_key=712f3b895ada9274714a881c2859b617',
                      $content, RequestInterface::METHOD_POST);
-                 dump();die();
+                 dump($response);die();
 
 
              }
